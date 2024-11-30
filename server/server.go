e8a27789b9d51236196baa155dba888aa50f1133
hejie_main@qq.com <hejie_main@qq.com>
2024-11-30 17:43:03 +0800

    logo

--- server/server.go
+++ server/server.go
@@ -1,11 +1,20 @@
 package server
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
+
+func printLogo() {
+	fmt.Println(`
+ _     __   __  ___   ___ 
+| |    \ \ / / / __| |_  )
+| |__   \ V /  \__ \  / /
+|____|   \_/   |___/ /___|`)
+}
 
 func Run(addr, dbfile, logfile string) {
 	initializeBase(dbfile, logfile)
@@ -15,5 +24,6 @@ func Run(addr, dbfile, logfile string) {
 	g.Use(cors.Default())
 	initializeRouter(g)
 	log.Println("server running on", addr)
+	printLogo()
 	log.Fatal(g.Run(addr))
 }
